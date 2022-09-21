87d3c75a8c49524a27e053924c7e5a17f43ceed4
Chang Woo Choi <changwoo92@gmail.com>
2022-09-21 15:04:38 -0700

    Initial Commit

---
+++ DisneyTakeHomeProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DisneyTakeHomeProject
+//
+//  Created by Chang Woo Choi on 9/21/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
